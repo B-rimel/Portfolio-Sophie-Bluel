762a0d2002a5b296de8e172c877ce46b6d1be52d
Luca <lucacampagna@hotmail.fr>
2023-05-10 00:43:26 +0200

    Mise en place des boutons de catégorie

--- FrontEnd/file.js
+++ FrontEnd/file.js
@@ -5,14 +5,36 @@ async function createElements() {
   console.log(projets);
   for (let i = 0; i < projets.length; i++) {
     const liste = projets[i];
+    const figureElement = document.createElement('figure');
     const imageElement = document.createElement('img');
     imageElement.src = liste.imageUrl;
-    const titreElement = document.createElement('h2');
+    const titreElement = document.createElement('figcaption');
     titreElement.innerHTML = liste.title;
-    const ficheWorks = document.querySelector(".divWorks");
-    ficheWorks.appendChild(imageElement);
-    ficheWorks.appendChild(titreElement);
+
+    const ficheWorks = document.querySelector(".gallery");
+    ficheWorks.appendChild(figureElement);
+    figureElement.appendChild(imageElement);
+    figureElement.appendChild(titreElement);
   }
 }
 
 createElements();
+
+
+async function createButtons() {
+  const response = await fetch('http://localhost:5678/api/categories');
+  let categories = await response.json();
+  console.log(categories);
+  for (let i = 0; i < categories.length; i++) {
+    const liste = categories[i];
+    const corpsBouton = document.createElement('button');
+    const texteBouton = document.createElement('p');
+    texteBouton.innerHTML = liste.name;
+
+    const boutonFiltre = document.querySelector(".filtres");
+    boutonFiltre.appendChild(corpsBouton);
+    corpsBouton.appendChild(texteBouton);
+    }
+}
+
+createButtons();
