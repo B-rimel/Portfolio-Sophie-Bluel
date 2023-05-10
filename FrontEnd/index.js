e22b79cbe96aef1f8306d0d3721188ede37c6b0e
Luca <lucacampagna@hotmail.fr>
2023-05-10 15:58:02 +0200

    Mise en place des filtres

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -4,26 +4,26 @@
 
   const getProjectsFromAPI = await fetch('http://localhost:5678/api/categories');
   let categories = await getProjectsFromAPI.json();
-  const listeCategories = categories.map(categories => categories.name);
 
 // Ces lignes affichent tout les projets dans la page à partir de getWorksFromAPI
-function genererListeProjets(){
-  for (let i = 0; i < projets.length; i++) {
-    const liste = projets[i];
+function genererListeProjets(projets){
+  const ficheWorks = document.querySelector(".gallery");
+  ficheWorks.innerHTML='';
+    for (const projet of projets) {
+    // const liste = projet;
     const figureElement = document.createElement('figure');
     const imageElement = document.createElement('img');
-    imageElement.src = liste.imageUrl;
+    imageElement.src = projet.imageUrl;
     const titreElement = document.createElement('figcaption');
-    titreElement.innerHTML = liste.title;
+    titreElement.innerHTML = projet.title;
 
-    const ficheWorks = document.querySelector(".gallery");
     ficheWorks.appendChild(figureElement);
     figureElement.appendChild(imageElement);
     figureElement.appendChild(titreElement);
   }
 }
 
-genererListeProjets();
+genererListeProjets(projets);
 
 
 
@@ -38,19 +38,22 @@ genererListeProjets();
 
 
 // Ces lignes crééent les boutons de filtre
-for (let i = 0; i < categories.length; i++) {
-  const liste = categories[i];
-  const corpsBouton = document.createElement('button');
-  const texteBouton = document.createElement('p');
-  texteBouton.innerHTML = liste.name;
+for (const category of categories) {
+  const bouton = document.createElement('button');
+  bouton.textContent = category.name;
 
   const boutonFiltre = document.querySelector("#filtres");
-  boutonFiltre.appendChild(corpsBouton);
-  corpsBouton.appendChild(texteBouton);
+  boutonFiltre.appendChild(bouton);
+
+  bouton.addEventListener('click', event => {
+    // console.log(event.target.textContent)
+    let filtered = category.id === 0 ? projets : projets.filter(projet => category.id === projet.categoryId);
+    console.log(filtered);
+    genererListeProjets(filtered);
+  })
 }
 
-function triProjet(list, categories) {
-  return listeCategories.filter((item) => item.categories === categories);
-}
-// Ces lignes servent à filtrer les projets par catégorie
-document.getElementById('filtres').addEventListener('click', triProjet);
+// function triProjet(list, category) {
+//   return listeCategories.filter((item) => item.categories === categories);
+// }
+
