dd72417f0b6b4144662787d6f7338698a6d5ab7a
B-rimel <lucacampagna@hotmail.fr>
2023-05-13 21:52:34 +0200

    Stockage des données utilisateurs

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,14 +1,30 @@
-const getLoginFromAPI = await fetch ('http://localhost:5678/api/users/login');
-    let loginCredential = getLoginFromAPI.json();
-
 //Ces lignes récupèrent les info de connexion de l'utilisateur
 const validerLogin = document.querySelector('.login-form');
-validerLogin.addEventListener('submit')
+validerLogin.addEventListener('submit');
 
-// Création de l’objet du nouvel avis.
 
-const avis = {
+const userLoginCredential = {
     email: event.target.querySelector("[name=e-mail").value,
     password: event.target.querySelector("[name=password]").value,
  };
  
+ const userLoginAPI = {
+    method : POST,
+    headers : { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userLoginCredential),
+ }
+
+ fetch('http://localhost:5678/api/users/login', userLoginAPI)
+ .then
+//  La méthode serait :
+//  -Récupérer les données saisies (eventlistener)
+//  -Envoyer ces données par la méthode POST :  
+//  verbe : POST;
+//  body : Charge utile, qui serait les données;
+//  header : content-type : application/json 
+// exemple : 
+// fetch("/pieces/1/avis", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: '{"commentaire": "Top produit !"}'
+// });
