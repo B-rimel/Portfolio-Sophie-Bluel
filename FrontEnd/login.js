1b72e6cf6c7ae8287d7378243744544a9cb8f238
Luca <lucacampagna@hotmail.fr>
2023-05-14 18:03:21 +0200

    Stockage de l'auth token dans localStorage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,39 +1,24 @@
-//Ces lignes récupèrent les info de connexion de l'utilisateur
-const validerLogin = document.querySelector('.login-form');
-validerLogin.addEventListener('submit', (userLogin) => {
-   userLogin.preventDefault();
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', async (event) => {
+   event.preventDefault();
    const userLoginCredential = {
-      email: userLogin.target.querySelector("[name=e-mail").value,
-      password: userLogin.target.querySelector("[name=password]").value,
+      email: event.target.querySelector("[name=e-mail").value,
+      password: event.target.querySelector("[name=password]").value,
    };
    console.log(userLoginCredential);
-});
 
-
-console.log(userLoginCredential);
-
-const userLoginAPI = {
-   method: POST,
-   headers: { 'Content-Type': 'application/json' },
-   body: JSON.stringify(userLoginCredential),
-}
-
-// fetch('http://localhost:5678/api/users/login', userLoginAPI)
-// .then(response => response.json())
-// if (response.hasOwnProperty("token")) {
-//    console.log('Le token existe');
-// }
-
-// else {
-//    console.log('Le token n\'existe pas.');
-// }
-
-fetch('http://localhost:5678/api/users/login', userLoginAPI)
-  .then(response => response.json())
-  .then(data => {
-    if (data.hasOwnProperty("token")) {
-      console.log('Le token existe');
-    } else {
-      console.log('Le token n\'existe pas.');
-    }
-  })
+   const userLoginAPI = {
+     method: 'POST',
+     headers: { 'Content-Type': 'application/json' },
+     body: JSON.stringify(userLoginCredential),
+   }
+     const response = await fetch('http://localhost:5678/api/users/login', userLoginAPI);
+     const loginCommand = await response.json();
+     if (response.ok) {
+       window.localStorage.setItem('token', loginCommand.token);
+       console.log(loginCommand.token)
+     } else {
+       alert('Le token n\'existe pas.');
+     }
+   }
+);
