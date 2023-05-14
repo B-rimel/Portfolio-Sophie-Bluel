a37bcc2ea8bf2952200405556e9f25bd4b68d998
Luca <lucacampagna@hotmail.fr>
2023-05-14 16:27:46 +0200

    Récupération de la réponse de l'API

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -19,23 +19,11 @@ const userLoginAPI = {
 }
 
 fetch('http://localhost:5678/api/users/login', userLoginAPI)
-.then(response => response.json())
-.then(data => {
-  const token = data.token;
-  localStorage.setItem('userToken', token);
-  console.log(userToken);
-})
-.catch(error => console.error(error));
+.then(response => response.json());
+if (response.hasOwnProperty("token")) {
+   console.log('Le token existe');
+}
 
-//  La méthode serait :
-//  -Récupérer les données saisies (eventlistener)
-//  -Envoyer ces données par la méthode POST :
-//  verbe : POST;
-//  body : Charge utile, qui serait les données;
-//  header : content-type : application/json
-// exemple :
-// fetch("/pieces/1/avis", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: '{"commentaire": "Top produit !"}'
-// });
+else {
+   console.log('Le token n\'existe pas.');
+}
