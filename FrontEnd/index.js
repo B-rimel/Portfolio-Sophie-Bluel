a90870d0f91bd127c2c35abb4141dfa025fa72a9
B-rimel <luca.afev78@gmail.com>
2023-05-24 17:48:55 +0200

    Création du contenu de la modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,17 +1,17 @@
 //  Ces lignes appellent les projets et les catégories dans l'API
- const getWorksFromAPI = await fetch('http://localhost:5678/api/works');
-  let projets = await getWorksFromAPI.json();
+const getWorksFromAPI = await fetch('http://localhost:5678/api/works');
+let projets = await getWorksFromAPI.json();
 
-  const getProjectsFromAPI = await fetch('http://localhost:5678/api/categories');
-  let categories = await getProjectsFromAPI.json();
+const getProjectsFromAPI = await fetch('http://localhost:5678/api/categories');
+let categories = await getProjectsFromAPI.json();
 
-  const token = sessionStorage.getItem('token');
-  console.log(token);
+const token = sessionStorage.getItem('token');
+console.log(token);
 // Ces lignes affichent tout les projets dans la page à partir de getWorksFromAPI
-function genererListeProjets(projets){
+function genererListeProjets(projets) {
   const ficheWorks = document.querySelector(".gallery");
-  ficheWorks.innerHTML='';
-    for (const projet of projets) {
+  ficheWorks.innerHTML = '';
+  for (const projet of projets) {
     // const liste = projet;
     const figureElement = document.createElement('figure');
     const imageElement = document.createElement('img');
@@ -30,41 +30,41 @@ genererListeProjets(projets);
 
 
 // Ces lignes ajoutent un bouton "toutes catégories" dans l'array de catégorie afin de l'avoir dans les boutons
-  const toutesCategories = {
-    id: 0,
-    name: 'Tous',
-  }
-  categories.unshift(toutesCategories);
-  console.log(categories);
+const toutesCategories = {
+  id: 0,
+  name: 'Tous',
+}
+categories.unshift(toutesCategories);
+console.log(categories);
 
 
 // Ces lignes crééent les boutons de filtre, seulement si l'utilisateur n'est pas connecté
-if (!token){
-for (const category of categories) {
-  const bouton = document.createElement('button');
-  bouton.textContent = category.name;
+if (!token) {
+  for (const category of categories) {
+    const bouton = document.createElement('button');
+    bouton.textContent = category.name;
 
-  const boutonFiltre = document.querySelector("#filtres");
-  boutonFiltre.appendChild(bouton);
+    const boutonFiltre = document.querySelector("#filtres");
+    boutonFiltre.appendChild(bouton);
 
-  bouton.addEventListener('click', event => {
-    let filtered = category.id === 0 ? projets : projets.filter(projet => category.id === projet.categoryId);
-    genererListeProjets(filtered);
-  })
-}
+    bouton.addEventListener('click', event => {
+      let filtered = category.id === 0 ? projets : projets.filter(projet => category.id === projet.categoryId);
+      genererListeProjets(filtered);
+    })
+  }
 }
 
 // Ces lignes affichent un bouton modifier dans la section introduction si l'utilisateur est connecté
-if(token){
+if (token) {
   const sectionHeader = document.querySelector('header');
   const header = document.createElement('div');
-  header.id='headerLogin';
+  header.id = 'headerLogin';
   const texteHeader = document.createElement('p');
   texteHeader.id = 'boutonModif';
   texteHeader.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>Mode édition';
   const boutonHeader = document.createElement('button');
   boutonHeader.innerHTML = 'Publier les changements';
-  
+
   sectionHeader.prepend(header);
   header.appendChild(texteHeader)
   header.appendChild(boutonHeader);
@@ -77,6 +77,7 @@ if(token){
 
   const sectionWorks = document.querySelector('#sectionProjet');
   const modifierWorks = document.createElement('p');
+  modifierWorks.id = 'boutonModale';
   modifierWorks.innerHTML = '<a href="#modal"><i class="fa-solid fa-pen-to-square"></i>Modifier</a>';
   modifierWorks.id = 'boutonModif';
   sectionWorks.appendChild(modifierWorks);
@@ -84,30 +85,72 @@ if(token){
 
 // Ces lignes changent le texte "login" en "logout" si l'utilisateur est connecté
 const boutonLogin = document.querySelector('#bouton_login');
-if(token){
+if (token) {
   boutonLogin.innerText = 'logout'
 }
 else {
   boutonLogin.innerText = 'login';
 }
 
+
+//Ces lignes affichent la modale 
+
 //Ces lignes affichent les projets dans la modale
-function fenetreModale1(projets){
-  const modaleWorks = document.querySelector("#modale1");
-  modaleWorks.innerHTML='';
-    for (const projet of projets) {
-    // const liste = projet;
-    const wrapperElement = document.createElement('div');
-    wrapperElement.id= 'works-editer';
-    const imageElement = document.createElement('img');
-    imageElement.src = projet.imageUrl;
-    const titreElement = document.createElement('p');
-    titreElement.innerText = 'éditer';
-    modaleWorks.appendChild(wrapperElement);
-    wrapperElement.appendChild(imageElement);
-    wrapperElement.appendChild(titreElement);
+// function fenetreModale1(projets){
+//   const modaleWorks = document.querySelector("#modale1");
+//   modaleWorks.innerHTML='';
+//     for (const projet of projets) {
+//     // const liste = projet;
+//     const wrapperElement = document.createElement('div');
+//     wrapperElement.id= 'works-editer';
+//     const imageElement = document.createElement('img');
+//     imageElement.src = projet.imageUrl;
+//     const titreElement = document.createElement('p');
+//     titreElement.innerText = 'éditer';
+//     modaleWorks.appendChild(wrapperElement);
+//     wrapperElement.appendChild(imageElement);
+//     wrapperElement.appendChild(titreElement);
+
+//   }
+// }
+
+
+function fenetreModale(projets) {
+  const modaleWorks = document.querySelector('main');
+
+
+  const modaleWrapper = document.createElement('aside');
+  modaleWrapper.id = 'modale-wrapper';
+  modaleWorks.appendChild(modaleWrapper);
+
+  const fenetreModale = document.createElement('div');
+  fenetreModale.id = 'modale-fenetre';
+  modaleWrapper.appendChild(fenetreModale);
+
+  const titrePremiereModale = document.createElement('h1');
+  titrePremiereModale.innerText = 'Galerie photo';
+  fenetreModale.appendChild(titrePremiereModale);
+
+  const grillePremiereModale = document.createElement('div');
+  grillePremiereModale.id = 'grille-modale';
+  fenetreModale.appendChild(grillePremiereModale);
+
+    const boutonFermer = document.createElement('i');
+    boutonFermer.className = 'fas fa-xmark';
+    fenetreModale.appendChild(boutonFermer);
+  for (const projet of projets) {
+
+    const elementGrille = document.createElement('div');
+    const imageGrille = document.createElement('img');
+      imageGrille.src = projet.imageUrl;
+    const texteGrille = document.createElement('p');
+      texteGrille.innerText = 'éditer';
+    grillePremiereModale.appendChild(elementGrille);
+    elementGrille.appendChild(imageGrille);
+    elementGrille.appendChild(texteGrille);
+
 
   }
 }
 
-fenetreModale1(projets);
+fenetreModale(projets);
