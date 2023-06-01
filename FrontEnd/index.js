fb19f1c753f7ba160a3480c12ceed7e6d8d60225
B-rimel <luca.afev78@gmail.com>
2023-06-01 20:22:55 +0200

    Implémentation de la deuxième modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -74,12 +74,6 @@ if (token) {
   modifierPhoto.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>Modifier';
   modifierPhoto.id = 'boutonModif';
   sectionIntro.appendChild(modifierPhoto);
-
-  const sectionWorks = document.querySelector('#sectionProjet');
-  const modifierWorks = document.createElement('p');
-  modifierWorks.id = 'boutonModale';
-  modifierWorks.innerHTML = '<a href="#modal"><i class="fa-solid fa-pen-to-square"></i>Modifier</a>';
-  sectionWorks.appendChild(modifierWorks);
 }
 
 // Ces lignes changent le texte "login" en "logout" si l'utilisateur est connecté
@@ -91,100 +85,4 @@ else {
   boutonLogin.innerText = 'login';
 }
 
-//Cette fonction affiche la modale
-function fenetreModale(projets) {
-  const modaleWorks = document.querySelector('main');
-
-
-  const modaleWrapper = document.createElement('aside');
-  modaleWrapper.id = 'modale-wrapper';
-  modaleWorks.appendChild(modaleWrapper);
-
-  const fenetreModale = document.createElement('div');
-  fenetreModale.id = 'modale-fenetre';
-  modaleWrapper.appendChild(fenetreModale);
-  fenetreModale.addEventListener('click', stopPropagation);
-  function stopPropagation(e){
-    e.stopPropagation();
-  }
-
-  const titrePremiereModale = document.createElement('h1');
-  titrePremiereModale.innerText = 'Galerie photo';
-  fenetreModale.appendChild(titrePremiereModale);
-
-  const grillePremiereModale = document.createElement('div');
-  grillePremiereModale.id = 'grille-modale';
-  fenetreModale.appendChild(grillePremiereModale);
-
-  const controlesModale = document.createElement('div');
-  controlesModale.id = 'controles-modale';
-  fenetreModale.appendChild(controlesModale);
-
-
-  const boutonDeuxiemeModale = document.createElement('button');
-  boutonDeuxiemeModale.innerText = 'Ajouter une photo';
-  controlesModale.appendChild(boutonDeuxiemeModale);
-
-  const boutonSupprimerGalerie = document.createElement('p');
-  boutonSupprimerGalerie.innerText = 'Supprimer la galerie';
-  boutonSupprimerGalerie.id = 'bouton-supprimer';
-  controlesModale.appendChild(boutonSupprimerGalerie);
-
-  const boutonFermer = document.createElement('i');
-  boutonFermer.className = 'fas fa-xmark';
-  fenetreModale.appendChild(boutonFermer);
-  boutonFermer.addEventListener('click', event => modaleWrapper.remove());
-  modaleWrapper.addEventListener('click', event => modaleWrapper.remove());
-
-
-
-//Cette boucle affiche les projets dans la modale
-  for (const projet of projets) {
-    const elementGrille = document.createElement('div');
-    elementGrille.dataset.id = projet.id;
-    elementGrille.id = 'element-grille';
-    const imageGrille = document.createElement('img');
-    imageGrille.src = projet.imageUrl;
-    const texteGrille = document.createElement('p');
-    texteGrille.innerText = 'éditer';
-    grillePremiereModale.appendChild(elementGrille);
-    elementGrille.appendChild(imageGrille);
-    elementGrille.appendChild(texteGrille);
-
-    const divBoutons = document.createElement('div');
-    divBoutons.id = 'boutons-modale';
-    elementGrille.appendChild(divBoutons);
-
-    const boutonEditer = document.createElement('p');
-    boutonEditer.innerHTML = '<i class="fa-solid fa-arrows-up-down-left-right"></i>';
-    divBoutons.appendChild(boutonEditer);
-    boutonEditer.style.display = 'none';
-
-    elementGrille.addEventListener('mouseover', function(event) {
-      boutonEditer.style.display = 'block';
-    }); 
-
-    elementGrille.addEventListener('mouseout', function(event){
-      boutonEditer.style.display = 'none';
-    });
-
-    const boutonSupprimer = document.createElement('p');
-    boutonSupprimer.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-    divBoutons.appendChild(boutonSupprimer);
-
-
-    boutonSupprimer.addEventListener('click', function(event){
-      const projetASupprimer = event.target.closest('#element-grille');
-      const projetId = projetASupprimer.dataset.id;
-      fetch(`http://localhost:5678/api/works/${projetId}`, {
-        method: 'DELETE',
-        headers: {'Authorization': `Bearer ${token}`}
-      })
-      .then(() => {
-        fenetreModale(projets);
-      })
-    })
-  }
-}
-
-boutonModale.addEventListener('click', event => fenetreModale(projets));
+export {token, projets, categories};
--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -1,36 +1,58 @@
+import {token, projets, categories} from './index.js';
+
+const sectionWorks = document.querySelector('#sectionProjet');
+const modifierWorks = document.createElement('p');
+modifierWorks.id = 'boutonModale';
+modifierWorks.innerHTML = '<a href="#modal"><i class="fa-solid fa-pen-to-square"></i>Modifier</a>';
+sectionWorks.appendChild(modifierWorks);
+
 function fenetreModale(projets) {
   const modaleWorks = document.querySelector('main');
-
 
   const modaleWrapper = document.createElement('aside');
   modaleWrapper.id = 'modale-wrapper';
   modaleWorks.appendChild(modaleWrapper);
 
-  const fenetreModale = document.createElement('div');
-  fenetreModale.id = 'modale-fenetre';
-  modaleWrapper.appendChild(fenetreModale);
-  fenetreModale.addEventListener('click', stopPropagation);
+  const modale = document.createElement('div');
+  modale.id = 'modale-fenetre';
+  modaleWrapper.appendChild(modale);
+  modale.addEventListener('click', stopPropagation);
   function stopPropagation(e){
     e.stopPropagation();
   }
 
   const titrePremiereModale = document.createElement('h1');
   titrePremiereModale.innerText = 'Galerie photo';
-  fenetreModale.appendChild(titrePremiereModale);
+  modale.appendChild(titrePremiereModale);
 
   const grillePremiereModale = document.createElement('div');
   grillePremiereModale.id = 'grille-modale';
-  fenetreModale.appendChild(grillePremiereModale);
+  modale.appendChild(grillePremiereModale);
+
+  const controlesModale = document.createElement('div');
+  controlesModale.id = 'controles-modale';
+  modale.appendChild(controlesModale);
+
+
+  const boutonDeuxiemeModale = document.createElement('button');
+  boutonDeuxiemeModale.innerText = 'Ajouter une photo';
+  controlesModale.appendChild(boutonDeuxiemeModale);
+  boutonDeuxiemeModale.addEventListener('click', event => fenetreModale2(modale, boutonFermer));
+
+  const boutonSupprimerGalerie = document.createElement('p');
+  boutonSupprimerGalerie.innerText = 'Supprimer la galerie';
+  boutonSupprimerGalerie.id = 'bouton-supprimer';
+  controlesModale.appendChild(boutonSupprimerGalerie);
 
   const boutonFermer = document.createElement('i');
   boutonFermer.className = 'fas fa-xmark';
-  fenetreModale.appendChild(boutonFermer);
+  modale.appendChild(boutonFermer);
   boutonFermer.addEventListener('click', event => modaleWrapper.remove());
   modaleWrapper.addEventListener('click', event => modaleWrapper.remove());
 
 
 
-
+//Cette boucle affiche les projets dans la modale
   for (const projet of projets) {
     const elementGrille = document.createElement('div');
     elementGrille.dataset.id = projet.id;
@@ -51,15 +73,14 @@ function fenetreModale(projets) {
     boutonEditer.innerHTML = '<i class="fa-solid fa-arrows-up-down-left-right"></i>';
     divBoutons.appendChild(boutonEditer);
     boutonEditer.style.display = 'none';
-    
+
     elementGrille.addEventListener('mouseover', function(event) {
       boutonEditer.style.display = 'block';
-    });
-    
-    elementGrille.addEventListener('mouseleave', function(event) {
+    }); 
+
+    elementGrille.addEventListener('mouseout', function(event){
       boutonEditer.style.display = 'none';
     });
-    
 
     const boutonSupprimer = document.createElement('p');
     boutonSupprimer.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
@@ -72,9 +93,47 @@ function fenetreModale(projets) {
       fetch(`http://localhost:5678/api/works/${projetId}`, {
         method: 'DELETE',
         headers: {'Authorization': `Bearer ${token}`}
-      }).then(fenetreModale(projets));
+      })
+      .then((response) => {  
+        if(response.ok){
+          projetASupprimer.remove();
+          const index = projets.findIndex(projet => projet.id == projetId);
+          projets.splice(index, 1);
+          genererListeProjets(projets);
+        }
+        else{
+          alert("Une erreur s'est produite lors de la suppression du projet");
+        }
+      })
     })
   }
 }
 
-// boutonModale.addEventListener('click', event => fenetreModale(projets));
+boutonModale.addEventListener('click', event => fenetreModale(projets));
+
+function fenetreModale2(modale, boutonFermer) {
+  modale.innerHTML = ''; //Cette ligne supprime les éléments de la modale
+  
+
+
+  const containerBoutons = document.createElement('div');
+  modale.appendChild(containerBoutons);
+  const boutonPrecedent = document.createElement('i');
+  boutonPrecedent.className = 'fa-solid fa-arrow-left';
+  containerBoutons.appendChild(boutonPrecedent);
+  containerBoutons.appendChild(boutonFermer);
+  boutonPrecedent.addEventListener('click', function() {
+    viderModale();
+    fenetreModale(projets);
+  });
+
+  const titreDeuxiemeModale = document.createElement('h1');
+  titreDeuxiemeModale.innerText = 'Ajout photo';
+  modale.appendChild(titreDeuxiemeModale);
+
+    function viderModale(modaleWrapper){
+      modaleWrapper.innerHTML = '';
+      modale.innerHTML = '';
+    }
+}
+
